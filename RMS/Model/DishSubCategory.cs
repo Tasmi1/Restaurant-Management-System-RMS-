224037f719eabc454df81ec82f1eb6e984a0ff1f
Shreeti Mool <shreeti@codebee.dk>
2021-07-12 18:55:00 +0545

    Created Model

---
+++ RMS/Model/DishSubCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model
+{
+    public class DishSubCategory
+    {
+        [Key]
+        public int SubCategoryID { get; set; }
+        [Required(ErrorMessage = "Sub Category Name is required")]
+        public string SubCategoryName { get; set; }
+        public int DishCategoryID { get; set; }
+    }
+}
---
+++ RMS/Model/UserType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model
+{
+    public class UserType
+    {
+        [Key]
+        public int UserTypeID { get; set; }
+        //[Required(ErrorMessage = "User Tyoe is required")]
+        //public int UserType { get; set; }
+    }
+}
