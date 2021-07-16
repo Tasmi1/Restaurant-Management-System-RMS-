b83a1c5647fe32360038b455dcae7cf4c5e50303
UlakS <srishti@codebee.dk>
2021-07-16 11:52:22 +0545

    RMS-T26 Index methode in inventory products controller

--- RMS/RMS/Controllers/InventoryProductsController.cs
+++ RMS/RMS/Controllers/InventoryProductsController.cs
@@ -14,7 +14,24 @@ namespace RMS.Controllers
     {
         private ResturantManagementDBEntities db = new ResturantManagementDBEntities();
 
-       
+        // GET: InventoryProducts
+        public ActionResult Index()
+        {          
+            var query = (from Ip in db.InventoryProducts
+                         join C in db.Categories on Ip.CategoryID equals C.CategoryID
+                         select new
+                         {
+                             Ip.InventoryProductID,
+                             Ip.ProductsName,
+                             C.CategoryName,
+                             Ip.ManufactureDate,
+                             Ip.ExpDate,
+                             Ip.Description
+                         });
+            return View(query.ToList());
+        }
+
+
 
 
        
