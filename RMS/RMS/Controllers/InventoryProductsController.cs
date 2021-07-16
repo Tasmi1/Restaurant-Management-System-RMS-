75edf85c0a45a9180fa18b0c1a4d1239ddfaa7fe
UlakS <srishti@codebee.dk>
2021-07-16 11:50:52 +0545

    RMS-T26 creating new  controller Inventory Products Controller

---
+++ RMS/RMS/Controllers/InventoryProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseLayer;
+
+namespace RMS.Controllers
+{
+    public class InventoryProductsController : Controller
+    {
+        private ResturantManagementDBEntities db = new ResturantManagementDBEntities();
+
+       
+
+
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
