af571dd6d483f7c49dfc9b9f69784d0cef1a2e26
UlakS <srishti@codebee.dk>
2021-07-16 11:21:55 +0545

    RMS-T20 craeting new contoller Category Controller

---
+++ RMS/RMS/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseLayer;
+
+namespace RMS.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ResturantManagementDBEntities db = new ResturantManagementDBEntities();
+
+       
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
