09c7a3f3fdf273c040dad29f61931d797181fc7c
Shreeti Mool <shreeti@codebee.dk>
2021-07-19 10:11:00 +0545

    Model

---
+++ RMS/RMS/Model/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        [Required(ErrorMessage = "Customer Name is required")]
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+    }
+}
---
+++ RMS/RMS/Model/Vendor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model
+{
+    public class Vendor
+    {
+        [Key]
+        public int VendorID { get; set; }
+        [Required(ErrorMessage = "Vendor Name is required")]
+        public string VendorName { get; set; }
+        [Required(ErrorMessage = "Bill Nu,ber is required")]
+        public string BillNumber { get; set; }
+        [Required(ErrorMessage = "Total Price is required")]
+        public string TotalPrice { get; set; }
+        [Required(ErrorMessage = "Purchase Date is required")]
+        public DateTime PurchaseDate { get; set; }
+
+
+    }
+}
