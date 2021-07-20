cc17a6b354f699c7235158fce55438a240e795cc
UlakS <srishti@codebee.dk>
2021-07-20 12:35:50 +0545

    RMS-T34 adding prop with validation in DTOs

--- RMS/RMS/Model/viewModes/UserTypeDTOs.cs
+++ RMS/RMS/Model/viewModes/UserTypeDTOs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,10 @@ namespace RMS.Model.viewModes
 {
     public class UserTypeDTOs
     {
+        public Guid UserTypeID { get; set; }
+        [Required(ErrorMessage = "User Type is required")]
+
+        [StringLength(95, ErrorMessage = "User Type Length should be between 3 to 9", MinimumLength = 3)]
+        public string Type { get; set; }
     }
 }
