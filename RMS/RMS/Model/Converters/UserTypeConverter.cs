392c54d816d8f0e251ecadd7ac92ba234b245e49
UlakS <srishti@codebee.dk>
2021-07-20 12:45:27 +0545

    RMS-T34 adding converter

--- RMS/RMS/Model/Converters/UserTypeConverter.cs
+++ RMS/RMS/Model/Converters/UserTypeConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RMS.Model.viewModes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,17 @@ namespace RMS.Model.Converters
 {
     public class UserTypeConverter
     {
+        public DatabaseLayer.UserType ConverToEntity(UserTypeDTOs model, DatabaseLayer.UserType userType)
+        {
+            userType.Type = model.Type;
+            return userType;
+        }
+        public UserTypeDTOs ConvertToModel(DatabaseLayer.UserType model)
+        {
+            UserTypeDTOs userType = new UserTypeDTOs();
+            userType.UserTypeID = model.UserTypeID;
+            userType.Type = model.Type;
+            return userType;
+        }
     }
 }
