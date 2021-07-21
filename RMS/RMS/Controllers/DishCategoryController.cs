f88e4a3716dd46f07e213cfc01e705a161da4c77
UlakS <srishti@codebee.dk>
2021-07-21 22:25:38 +0545

    RMS-T39 creating controller of dish category

---
+++ RMS/RMS/Controllers/DishCategoryController.cs
@@ -0,0 +1,78 @@
+﻿using DatabaseLayer;
+using RMS.Model.Services;
+using RMS.Model.viewModes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RMS.Controllers
+{
+    public class DishCategoryController : Controller
+    {
+        // GET: DishCategory
+        private readonly DishCategoryService dishCategoryService = new DishCategoryService();
+        public ActionResult Index()
+        {
+            var dishCategories = dishCategoryService.GetAll();
+            return View(dishCategories);
+        }
+        public ActionResult Create()
+        {
+            DishCategoryDTOs model = new DishCategoryDTOs();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Create(DishCategoryDTOs model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool result = dishCategoryService.Create(model);
+                if (result == true)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
+        }
+        public ActionResult Edit(Guid id)
+        {
+            DishCategoryDTOs model = dishCategoryService.GetById(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(DishCategoryDTOs model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool result = dishCategoryService.Update(model);
+                if (result)
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View(model);
+        }
+
+        public ActionResult Details(Guid id)
+        {
+            DishCategoryDTOs model = dishCategoryService.GetById(id);
+            return View(model);
+        }
+
+
+        public ActionResult Delete(Guid id)
+        {
+
+            ResturantManagementDBEntities db = new ResturantManagementDBEntities();
+            {
+                var model = db.UserTypes.Find(id);
+                db.UserTypes.Remove(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
