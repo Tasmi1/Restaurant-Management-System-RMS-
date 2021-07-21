866485ab8cd3e8036006b17d7da3de2a541b3862
Shreeti Mool <shreeti@codebee.dk>
2021-07-21 17:00:52 +0545

    Menu Edit, Create and Index

---
+++ RMS/RMS/Controllers/MenuController.cs
@@ -0,0 +1,63 @@
+﻿using RMS.Model.Services;
+using RMS.Model.viewModes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RMS.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly MenuService menuServie = new MenuService();
+        public ActionResult Index()
+        {
+            var menus = menuServie.GetAll();
+            return View(menus);
+        }
+
+        public ActionResult Create()
+        {
+
+            MenuDTOs model = new MenuDTOs();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(MenuDTOs model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool result = menuServie.Create(model);
+                if (result)
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View(model);
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            MenuDTOs model = menuServie.GetById(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(MenuDTOs model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool result = menuServie.Update(model);
+                if (result)
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View(model);
+        }
+    }
+}
---
+++ RMS/RMS/Model/Converters/MenuConverter.cs
@@ -0,0 +1,29 @@
+﻿using RMS.Model.viewModes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model.Converters
+{
+    public class MenuConverter
+    {
+        public DatabaseLayer.Menu ConverToEntity(MenuDTOs model, DatabaseLayer.Menu menu)
+        {
+            menu.MenuName = model.MenuName;
+            menu.MenuPrice = model.MenuPrice;
+            return menu;
+        }
+        public MenuDTOs ConvertToModel(DatabaseLayer.Menu model)
+        {
+            MenuDTOs menu = new MenuDTOs();
+            menu.MenuID = model.MenuID;
+            menu.MenuName = model.MenuName;
+            menu.MenuPrice = model.MenuPrice;
+            //menu.DishCategoryID = (Guid)model.DishCategoryID;
+
+            return menu;
+        }
+
+    }
+}
