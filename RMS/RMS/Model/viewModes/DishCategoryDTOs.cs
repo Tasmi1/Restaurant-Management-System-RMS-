4c9838c37470ccb439285d8d3affff942fdf4868
UlakS <srishti@codebee.dk>
2021-07-21 22:07:47 +0545

    RMS-T39 creating dtos of dish category

--- RMS/RMS/Model/viewModes/DishCategoryDTOs.cs
+++ RMS/RMS/Model/viewModes/DishCategoryDTOs.cs
@@ -4,13 +4,13 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace RMS.Model
+namespace RMS.Model.viewModes
 {
-    public class DishCategory
+    public class DishCategoryDTOs
     {
-        [Key]
         public int DishCategoryID { get; set; }
         [Required(ErrorMessage = "Dish Category Name is required")]
+        [StringLength(95, ErrorMessage = "Dish Category  Length should be between 3 to 95", MinimumLength = 3)]
         public string DishCategoryName { get; set; }
     }
 }
