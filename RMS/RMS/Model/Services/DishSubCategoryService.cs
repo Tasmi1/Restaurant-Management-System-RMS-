e364ad752e0ef1c1dac6014ea07b37b462554f10
Tasmiya Fatema <tasmiya@codebee.dk>
2021-07-22 17:09:31 +0545

    RMS-T-41 dishsubcategory CRUD

---
+++ RMS/RMS/Model/Services/DishSubCategoryService.cs
@@ -0,0 +1,113 @@
+﻿using DatabaseLayer;
+using RMS.Model.Converters;
+using RMS.Model.viewModes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model.Services
+{
+    public class DishSubCategoryService
+    {
+
+        private readonly DishSubCategoryConverter converter = new DishSubCategoryConverter();
+
+        public bool Create(DishSubCategoryDTOs dishSub)
+        {
+            try
+            {
+                using(ResturantManagementDBEntities db = new ResturantManagementDBEntities())
+                {
+                    DishSubCategory dishSubCategory = new DishSubCategory();
+                    dishSubCategory.SubCategoryID = Guid.NewGuid();
+                    dishSubCategory = converter.ConvertToEntity(dishSub, dishSubCategory);
+
+                    db.DishSubCategories.Add(dishSubCategory);
+                    db.SaveChanges();
+                    return true;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        
+
+        public bool Update(DishSubCategoryDTOs dishSub)
+        {
+            try
+            {
+                using(ResturantManagementDBEntities db = new ResturantManagementDBEntities())
+                {
+                    DishSubCategory dishSubCategory = db.DishSubCategories.FirstOrDefault(dsc => dsc.SubCategoryID == dishSub.SubCategoryId);
+                    if(dishSubCategory != null)
+                    {
+                        dishSubCategory = converter.ConvertToEntity(dishSub, dishSubCategory);
+                        db.SaveChanges();
+                        return true;
+
+                    }
+                    return false;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public DishSubCategoryDTOs GetById(Guid dishSubCategoryId)
+        {
+            DishSubCategoryDTOs model = new DishSubCategoryDTOs();
+            try
+            {
+                using (ResturantManagementDBEntities db = new ResturantManagementDBEntities())
+                {
+
+                    DishSubCategory dishSubCategory = db.DishSubCategories.FirstOrDefault(c => c.SubCategoryID == dishSubCategoryId);
+                    if (dishSubCategory != null)
+                    {
+                        model = converter.ConvertToModel(dishSubCategory);
+
+                    }
+                    return model;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+        public List<DishSubCategoryDTOs> GetAll()
+        {
+            List<DishSubCategoryDTOs> dishsubCategories = new List<DishSubCategoryDTOs>();
+            try
+            {
+                using (ResturantManagementDBEntities db = new ResturantManagementDBEntities())
+                {
+
+                    var dbDishSubCategorys = db.DishSubCategories.ToList();
+                    foreach (var dishsubcategory in dbDishSubCategorys)
+                    {
+                        dishsubCategories.Add(converter.ConvertToModel(dishsubcategory));
+
+                    }
+                    return dishsubCategories;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ RMS/RMS/Model/viewModes/DishSubCategoryDTOs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model.viewModes
+{
+    public class DishSubCategoryDTOs
+    {
+
+        public Guid SubCategoryId { get; set; }
+
+        [Required (ErrorMessage="Sub Category name is required")]
+        [StringLength(95, ErrorMessage = "Dish Category  Length should be between 3 to 95", MinimumLength = 3)]
+        public string SubCategoryName { get; set; }
+
+        public Guid DishCategoryId { get; set; }
+
+        
+    }
+}
