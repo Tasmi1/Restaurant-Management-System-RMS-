5b5e0808b7254a224407f7e45f44c806d35958a8
Tasmiya Fatema <tasmiya@codebee.dk>
2021-07-27 10:55:10 +0545

    Edit changes

--- RMS/RMS/Controllers/DishSubCategoryController.cs
+++ RMS/RMS/Controllers/DishSubCategoryController.cs
@@ -75,8 +75,8 @@ namespace RMS.Controllers
 
             ResturantManagementDBEntities db = new ResturantManagementDBEntities();
             {
-                var dishsub = db.UserTypes.Find(id);
-                db.UserTypes.Remove(dishsub);
+                var dishsub = db.DishSubCategories.Find(id);
+                db.DishSubCategories.Remove(dishsub);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
