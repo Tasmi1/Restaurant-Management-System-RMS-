b7a9a3dd807bc5ffe4cb6f0aef14087b70843635
Shreeti Mool <shreeti@codebee.dk>
2021-08-05 12:30:57 +0545

    RMS-T54 Database change

---
+++ RMS/RMS/Model/Converters/InvoiceConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model.Converters
+{
+    public class InvoiceConverter
+    {
+        public Invoice ConvertToEntity(InvoiceDTOs model, Invoice invoice)
+        {
+
+        }
+    }
+}
---
+++ RMS/RMS/Model/viewModes/InvoiceDTOs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model.viewModes
+{
+    public class InvoiceDTOs
+    {
+        public Guid InvoiceID { get; set; }
+        [StringLength(50, ErrorMessage = "Name Length should be between 3 to 50", MinimumLength = 3)]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Created Date is required")]
+        public DateTime CreatedDate { get; set; }
+        [Required(ErrorMessage = "Created Time is required")]
+        public TimeSpan CreatedTime { get; set; }
+        [Required(ErrorMessage = "Sub Total is required")]
+        public bool Status { get; set; }
+    }
+}
