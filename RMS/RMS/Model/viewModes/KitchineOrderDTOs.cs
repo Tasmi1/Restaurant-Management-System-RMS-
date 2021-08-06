cb7fa246a2bc1553569ee5cd83426c340e263b77
UlakS <srishti@codebee.dk>
2021-08-06 09:53:52 +0545

    RMS-T53 adding dtos

---
+++ RMS/RMS/Model/viewModes/KitchineOrderDTOs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model.viewModes
+{
+    public class OrderCartDTOs
+    {
+        public int OrderCartID { get; set; }
+        public System.DateTime OrderDate { get; set; }
+        public string OrderNumber { get; set; }      
+        public bool OrderStatus { get; set; }
+
+
+    }
+}
