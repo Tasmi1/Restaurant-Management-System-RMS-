39ea737811259694c96b854cc7a72ae359cd7e9d
UlakS <srishti@codebee.dk>
2021-08-10 15:22:50 +0545

    RMS-T56 adding method service class

--- RMS/RMS/Model/Services/KitchineOrderService.cs
+++ RMS/RMS/Model/Services/KitchineOrderService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DatabaseLayer;
+using RMS.Model.Converters;
+using RMS.Model.viewModes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +10,30 @@ namespace RMS.Model.Services
 {
     public class KitchineOrderService
     {
+        private readonly KitchineOrderConverter converter = new KitchineOrderConverter();
+
+        public List<KitchineOrderDTOs> GetAll()
+        {
+            List<KitchineOrderDTOs> orders = new List<KitchineOrderDTOs>();
+            try
+            {
+                using (ResturantManagementDBEntities db = new ResturantManagementDBEntities())
+                {
+
+                    var dbOrder = db.OrderCarts.ToList();
+                    foreach (var order in dbOrder)
+                    {
+                        orders.Add(converter.ConvertToModel(order));
+
+                    }
+                    return orders;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
     }
 }
