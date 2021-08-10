e2afe4f5dfe39d4141e055ffd1b8a32149d4d281
Tasmiya Fatema <tasmiya@codebee.dk>
2021-08-10 13:54:05 +0545

    conflict

--- RMS/RMS/Model/Converters/OrderConverter.cs
+++ RMS/RMS/Model/Converters/OrderConverter.cs
@@ -13,16 +13,9 @@ namespace RMS.Model.Converters
             order.OrderTime = model.OrderTime;
             order.OrderDate = model.OrderDate;
             order.CustomerID = model.CustomerID;
-<<<<<<< HEAD
-           /* order.MenuID = model.MenuID;*/
-            order.InventoryProductID = model.InventoryProductID;
-=======
-            order.MenuID = model.MenuID;
-            order.
->>>>>>> 6b70a72ec2f3630e298a251191f166ccf8ba2513
-            
 
-            //order.Menu.MenuName = model.MenuName;
+            order.InventoryProductID = model.InventoryProductID;
+
          
             return order;
         }
@@ -37,7 +30,7 @@ namespace RMS.Model.Converters
             
             
             order.CustomerID = model.CustomerID;
-           /* order.MenuID = model.MenuID;*/
+           
             order.InventoryProductID = model.InventoryProductID;
             return order;
         }
