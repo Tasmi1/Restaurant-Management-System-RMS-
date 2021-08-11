4de6c487c912816c34ae984daf524df5b15f9392
Bijay <bijay@codebee.com>
2021-08-11 12:27:56 +0545

    commit

---
+++ RMS/RMS/Model/viewModes/BookingDTOs.cs
@@ -0,0 +1,20 @@
+﻿using RMS.Model.viewModes.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model.viewModes
+{
+    public class BookingDTOs
+    {
+        public Guid BookingID { get; set; }
+        public DateTime Date { get; set; }
+        public string TableNumber { get; set; }
+        public string Description { get; set; }
+        public Guid CustometID { get; set; }
+
+    }
+
+
+}
