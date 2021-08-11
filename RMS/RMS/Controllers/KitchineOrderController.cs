aee75863a3a1afbb9168d30310285268f19a8b15
UlakS <srishti@codebee.dk>
2021-08-11 15:17:01 +0545

    RMS-T56 addinf kot

--- RMS/RMS/Controllers/KitchineOrderController.cs
+++ RMS/RMS/Controllers/KitchineOrderController.cs
@@ -1,4 +1,5 @@
-﻿using RMS.Model.Services;
+﻿using DatabaseLayer;
+using RMS.Model.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,33 @@ namespace RMS.Controllers
         {
             var order = orderService.GetAll();
             return View(order);
+        }
+        //        SELECT* FROM CartDetails
+        //INNER JOIN OrderCart o ON o.OrderCartID = CartDetails.OrderCartID
+        //INNER JOIN Menu m ON CartDetails.MenuID = m.MenuID;
+
+
+        public ActionResult OrderDetails()
+        {
+            using (ResturantManagementDBEntities db = new ResturantManagementDBEntities())
+            {
+                List<OrderCart> order = db.OrderCarts.ToList();
+                List<CartDetail> orderItems = db.CartDetails.ToList();
+                List<Menu> menus = db.Menus.ToList();
+
+                var employeeRecord = from o in order
+                                     join d in orderItems on e.Department_Id equals d.DepartmentId into table1
+                                     from d in table1.ToList()
+                                     join i in incentives on e.Incentive_Id equals i.IncentiveId into table2
+                                     from i in table2.ToList()
+                                     select new ViewModel
+                                     {
+                                         employee = e,
+                                         department = d,
+                                         incentive = i
+                                     };
+                return View(employeeRecord);
+            }
         }
     }
 }
