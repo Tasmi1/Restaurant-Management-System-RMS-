c6737bc37ab64e0bdab65323c188ded6b15c5718
UlakS <srishti@codebee.dk>
2021-08-12 10:52:11 +0545

    update database

--- RMS/DatabaseLayer/OrderItem.cs
+++ RMS/DatabaseLayer/OrderItem.cs
@@ -19,7 +19,9 @@ namespace DatabaseLayer
         public Nullable<int> Quantity { get; set; }
         public string Price { get; set; }
         public string Description { get; set; }
+        public System.Guid InventoryProductID { get; set; }
     
+        public virtual InventoryProduct InventoryProduct { get; set; }
         public virtual Order Order { get; set; }
     }
 }
