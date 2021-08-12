582655bf6ab07046c7a9491f923bdae38954a070
UlakS <srishti@codebee.dk>
2021-08-12 11:18:24 +0545

    update datbase

--- RMS/DatabaseLayer/Properties/AssemblyInfo.cs
+++ RMS/DatabaseLayer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b79e9fa3-4195-42bb-9869-1ae93b4b7fc6")]
+[assembly: Guid("0a97baef-07f1-444e-b444-6229fa24e65b")]
 
 // Version information for an assembly consists of the following four values:
 //
