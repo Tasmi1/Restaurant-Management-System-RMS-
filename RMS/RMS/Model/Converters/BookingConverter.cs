14e8c7675af3262c98788713c3b4febd4d891fd4
Bijay <bijay@codebee.com>
2021-08-12 10:33:13 +0545

    chnage in booking file

---
+++ RMS/RMS/Model/Converters/BookingConverter.cs
@@ -0,0 +1,32 @@
+﻿using DatabaseLayer;
+using RMS.Model.viewModes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Model.Converters
+{
+    public class BookingConverter
+    {
+        public DatabaseLayer.Booking ConverToEntity(BookingDTOs model, DatabaseLayer.Booking booking)
+        {
+            booking.Date = model.Date;
+            booking.TableNumber = model.TableNumber;
+            booking.Description = model.Description;
+            booking.CustomerID = model.CustomerID;
+            return booking;
+        }
+        public BookingDTOs ConvertToModel(DatabaseLayer.Booking model)
+        {
+            BookingDTOs booking = new BookingDTOs();
+            booking.BookingID = model.BookingID;
+            booking.Date = model.Date;
+            booking.TableNumber = model.TableNumber;
+            booking.Description = model.Description;
+            booking.CustomerID = model.CustomerID;
+            return booking;
+        }
+
+    }
+}
